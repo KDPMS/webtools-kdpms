b908688b0922338d5f11ce4aa006f22830e0f2cc
KDPMS <kdmps.playstore@gmail.com>
2020-02-17 14:13:01 +0700

    rev

--- application/views/dashboard_kpi_ao.php
+++ application/views/dashboard_kpi_ao.php
@@ -551,7 +551,6 @@
 
 
 		new cchart2('#modal_bz', '#dt_tables_bz', 6, 'DATA BZ', [0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]);
-		// new cchart2('#modal_cr', '#dt_tables_cr', 6, 'DATA CR', [0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]);
 		//tutup datatable
 
 		//alert data tidak ada
